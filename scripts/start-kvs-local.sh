1e8a3ad792ffa54016f40a58a93f01d887583b71
vsreekanti <vsreekanti@gmail.com>
2018-08-03 17:57:09 +0000

    Moves !/bin/bash declaration above license

--- scripts/start-kvs-local.sh
+++ scripts/start-kvs-local.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #  Copyright 2018 U.C. Berkeley RISE Lab
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,8 +13,6 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-
-#!/bin/bash
 
 if [ -z "$1" ] && [ -z "$2" ]; then
   echo "Usage: ./scripts/start_local.sh build start-user"
