3561f6235f4274d591001427b924b2299a066678
TaraMirmira <tmirmira@gmail.com>
2018-10-04 01:37:33 +0000

    fix threads being inserted in node_join_handler

--- kvs/src/kvs/node_join_handler.cpp
+++ kvs/src/kvs/node_join_handler.cpp
@@ -112,9 +112,13 @@ void node_join_handler(
                       std::find(threads.begin(), threads.end(), wt) == 
                                 threads.end())) {
             join_remove_set.insert(key);
-            ServerThread first_thread = threads.front();
-            join_addr_keyset_map[first_thread.get_gossip_connect_addr()].insert(
-                key);
+
+            int local_rep = placement[key].local_replication_map_[tier];
+            for (int i = 0; i < local_rep; i++) {
+              ServerThread thread = threads[i];
+              join_addr_keyset_map[thread.get_gossip_connect_addr()].insert(
+                  key);
+            }
           }
         } else {
           logger->error(
