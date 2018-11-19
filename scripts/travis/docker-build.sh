9e9b14e542d8c97de47e03c21036cb189cda1d45
Vikram Sreekanti <vsreekanti@gmail.com>
2018-11-19 14:37:08 -0800

    Adds script to build new docker image when building master

--- scripts/travis/docker-build.sh
+++ scripts/travis/docker-build.sh
@@ -1,46 +1,28 @@
+#!/bin/bash
+
 #  Copyright 2018 U.C. Berkeley RISE Lab
-# 
+#
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
 #  You may obtain a copy of the License at
-# 
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS,
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-language: cpp
-sudo: required
+# only build a new Docker image if this is a master branch build -- ignore this
+# for PR builds
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+  cd dockerfiles
+  docker build . -f anna.dockerfile -t fluentproject/annakvs
+  docker build . -f kops.dockerfile -t fluentproject/kops
 
-os: 
-  - linux
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-dist: trusty
-
-compiler:
-  - clang
-
-services:
-  - docker
-
-env:
-  global:
-    - PROTOBUF_DIR="$HOME/protobuf"
-    - LCOV_VERSION=1.13
-
-cache:
-  directories:
-    - $PROTOBUF_DIR
-
-install:
-  - ./scripts/travis/travis-install.sh
-
-script:
-  - ./scripts/travis/travis-build.sh
-
-after_success:
-  - ./scripts/travis/upload-codecov.sh
-  - ./scripts/travis/docker-build.sh
+  docker push fluentproject/annakvs
+  docker push fluentproject/kops
+fi
