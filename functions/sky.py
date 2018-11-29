a42b7d5229ddc9b3a5fe02485f760346db180edd
Vikram Sreekanti <vsreekanti@gmail.com>
2018-11-29 10:48:14 -0800

    Adds basic function serving framework

---
+++ functions/sky.py
@@ -0,0 +1,81 @@
+from anna.client import AnnaClient
+import cloudpickle as cp
+import boto3
+import requests
+import utils
+
+class SkyConnection():
+    def __init__(self, func_addr, kvs_addr, port=6000):
+        self.service_addr = 'http://'+  addr + ":" + str(port)
+        self.kvs_client = AnnaClient(kvs_addr)
+
+        self.session = requests.Session()
+
+    def list(self, prefix=None):
+        for fname in self._get_func_list(prefix):
+            print(fname)
+
+    def get(self, name):
+        if name not in self._get_func_list():
+            print("No function found with name '" + name + "'.")
+            print("To view all functions, use the `list` method.")
+            return None
+
+        return SkyFunc(name, self._name_to_handle(name), self, self.kvs_client)
+
+    def _get_func_list(self, prefix=None):
+        if prefix == None:
+            r = self.session.get(self.service_addr + "/list")
+        else:
+            r = self.session.get(self.service_addr + "/list/" + prefix)
+        return cp.loads(r.content)
+
+
+    def _name_to_handle(self, name):
+        return name
+
+    def exec_func(self, handle, args):
+        args_bin = cp.dumps(args)
+
+        r = self.session.post(self.service_addr + "/" + handle, data=args_bin)
+        return cp.loads(r.content)
+
+    def register(self, func, name):
+        self.session.post(self.service_addr + "/create/" + name,
+                data=cp.dumps(func))
+
+    def deregister(self, name):
+        self.session.post(self.service_addr + "/remove/" + name)
+
+
+class SkyFuture():
+    def __init__(self, obj_id, kvs_client):
+        self.obj_id = obj_id
+        self.kvs_client = kvs_client
+
+    def get(self):
+        obj = self.kvs_client.get(self.obj_id)
+
+        while not obj:
+            obj = self.kvs_client.get(self.obj_id)
+
+        return cp.loads(obj)
+
+class SkyFunc():
+    def __init__(self, name, func_handle, conn, kvs_client):
+        self.name = name
+        self.handle = func_handle
+        self._conn = conn
+        self._kvs_client = kvs_client
+
+    def __call__(self, *args):
+        obj_id = self._conn.exec_func(self.handle, args)
+        return SkyFuture(obj_id, self._kvs_client)
+
+class SkyReference():
+    def __init__(self, key, deserialize):
+        self.key = key
+        self.deserialize = deserialize
+
+def connect(addr=None, port=7000, pr_type='FS', pr_info={'dir': 'funcs/'}):
+    return SkyConnection(addr, port, pr_type, pr_info)
---
+++ functions/sky_server.py
@@ -0,0 +1,113 @@
+#  Copyright 2018 U.C. Berkeley RISE Lab
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from anna.client import AnnaClient
+import cloudpickle as cp
+import flask
+from flask import session
+from flask_session import Session
+import os
+from threading import Thread
+import utils
+import uuid
+
+app = flask.Flask(__name__)
+
+routing_addr = os.environ['ROUTE_ADDR']
+client = AnnaClient(routing_addr)
+
+@app.route('/create/<funcname>', methods=['POST'])
+def create_func(funcname):
+    func_binary = flask.request.get_data()
+
+    app.logger.info('Creating function: ' + funcname + '.')
+    client.put(funcname, func_binary)
+
+    return construct_response()
+
+@app.route('/remove/<funcname>', methods=['POST'])
+def remove_func(funcname):
+    app.logger.info('Removing function: ' + funcname + '.')
+    client.remove(funcname)
+
+    return construct_response()
+
+@app.route('/<funcname>', methods=['POST'])
+def call_func(funcname):
+    obj_id = str(uuid.uuid4())
+    t = Thread(target=_exec_func, args=(funcname, obj_id, flask.request.get_data()))
+    t.start()
+
+    return construct_response(obj_id)
+
+def _exec_func(funcname, obj_id, arg_obj):
+    func_binary = client.get(funcname)
+    func = cp.loads(func_binary)
+
+    args = cp.loads(arg_obj)
+
+    func_args = ()
+
+    for arg in args:
+        if isinstance(arg, sky.SkyReference):
+            func_args = (_resolve_ref(arg, client),)
+        else:
+            func_args += (arg,)
+
+
+    res = func(*args)
+
+    client.put(obj_id, cp.dumps(res))
+
+def _resolve_ref(ref, client):
+    ref_data = client.get_object(ref.key)
+
+    if ref.deserialize:
+        return cp.loads(ref_data)
+    else:
+        return ref_data
+
+@app.route('/list', methods=['GET'])
+@app.route('/list/<prefix>', methods=['GET'])
+def list_funcs(prefix=''):
+    result = client.get_list(prefix)
+
+    return construct_response(result)
+
+def construct_response(obj=None):
+    resp = flask.make_response()
+    if obj != None:
+        resp.data = cp.dumps(obj)
+        resp.content_type = 'text/plain'
+
+    resp.status_code = 200
+
+    return resp
+
+def return_error(error=''):
+    resp = flask.make_response()
+    if error != '':
+        resp.data = error
+        resp.content_type = 'text/plain'
+
+    resp.status_code = 400
+
+    return resp
+
+def run():
+    app.secret_key = "this is a secret key"
+    Session(app)
+    app.run(threaded=True, host='0.0.0.0', port=7000)
+
+run()
