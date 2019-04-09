44bd96a1c3c13a29272797d6e0e05c7f552aac4b
cw75 <cgwu0530@gmail.com>
2019-04-09 07:58:15 +0000

    add simple tests for causal utils

---
+++ functions/cache/tests/causal_cache_base.hpp
@@ -0,0 +1,39 @@
+//  Copyright 2018 U.C. Berkeley RISE Lab
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#include "kvs/tests/mock/mock_utils.hpp"
+#include "causal_cache_utils.hpp"
+
+MockZmqUtil mock_zmq_util;
+ZmqUtilInterface* kZmqUtil = &mock_zmq_util;
+
+logger log_ = spdlog::basic_logger_mt("mock_log", "mock_log.txt", true);
+
+class CausalCacheTest : public ::testing::Test {
+ protected:
+
+  CausalCacheTest() {}
+
+  virtual ~CausalCacheTest() {}
+
+ public:
+  void SetUp() {}
+
+  void TearDown() {
+    // clear all the logged messages after each test
+    mock_zmq_util.sent_messages.clear();
+  }
+
+  vector<string> get_zmq_messages() { return mock_zmq_util.sent_messages; }
+};
--- kvs/tests/mock/mock_utils.hpp
+++ kvs/tests/mock/mock_utils.hpp
@@ -12,10 +12,10 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-#ifndef TESTS_MOCKED_HPP_
-#define TESTS_MOCKED_HPP_
+#ifndef KVS_TESTS_MOCKED_HPP_
+#define KVS_TESTS_MOCKED_HPP_
 
-#include "hash_ring.hpp"
+#include "kvs/include/hash_ring.hpp"
 #include "zmq/zmq_util.hpp"
 
 class MockZmqUtil : public ZmqUtilInterface {
@@ -37,4 +37,4 @@ class MockHashRingUtil : public HashRingUtilInterface {
       const vector<unsigned>& tier_ids, bool& succeed, unsigned& seed);
 };
 
-#endif  // TESTS_MOCKED_HPP_
+#endif  // KVS_TESTS_MOCKED_HPP_
