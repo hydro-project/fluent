fe9cb54b1ed905c6f507a7890860ddd71a73abbb
saurav_c <sauravc@berkeley.edu>
2019-04-12 16:57:14 -0700

    Formatting fix

--- scripts/start-kvs-local.sh
+++ scripts/start-kvs-local.sh
@@ -40,6 +40,5 @@ echo $RPID >> pids
 echo $SPID >> pids
 
 if [ "$2" = "y" ] || [ "$2" = "yes" ]; then
-  # ./build/kvs/src/cli/flkvs-cli
-  ./build/client/kvs/cpp/flkvs-cli conf/kvs-config.yml
+  ./build/kvs/client/cpp/flkvs-cli conf/kvs-config.yml
 fi
