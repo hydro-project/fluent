1dce3a85e3fc011d3b08c6c4da92a2693b41215b
cw75 <cgwu0530@gmail.com>
2019-04-14 22:01:59 +0000

    minor

--- functions/cache/src/causal_cache_kvs_response_handler.cpp
+++ functions/cache/src/causal_cache_kvs_response_handler.cpp
@@ -62,7 +62,7 @@ void kvs_response_handler(
                        client_id_to_address_map);
     } else {
       if (request_id_to_address_map.find(response.response_id()) ==
-          request_id_to_address_map.end()) {
+          request_id_to_address_map.end() && response.tuples(0).lattice_type() != LatticeType::LWW) {
         log->error("Missing request id - address entry for this PUT response");
       } else {
         CausalResponse resp;
