f54cef09a43630c5df9a3727f0ed5ae61bd6c3c3
cw75 <cgwu0530@gmail.com>
2019-04-15 05:06:39 +0000

    typo fix

--- kvs/client/python/anna/ipc_client.py
+++ kvs/client/python/anna/ipc_client.py
@@ -123,7 +123,7 @@ class IpcAnnaClient:
             request.versioned_key_locations[addr].versioned_keys.extend(
                                 versioned_key_locations[addr].versioned_keys)
 
-        for keys in keys:
+        for key in keys:
             tp = request.tuples.add()
             tp.key = key
 
