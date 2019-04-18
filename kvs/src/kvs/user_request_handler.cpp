fd8799f8f19c93f84a3cd838cacbd6aeb19e3e92
cw75 <cgwu0530@gmail.com>
2019-04-18 06:26:44 +0000

    remove some logging

--- kvs/src/kvs/user_request_handler.cpp
+++ kvs/src/kvs/user_request_handler.cpp
@@ -51,10 +51,6 @@ void user_request_handler(
         global_hash_rings, local_hash_rings, key_replication_map, pushers,
         kSelfTierIdVector, succeed, seed);
 
-    for (const auto& thread : threads) {
-      log->error("Thread {} is responsible for key {}.", thread.public_ip(), key);
-    }
-
     if (succeed) {
       if (std::find(threads.begin(), threads.end(), wt) == threads.end()) {
         if (is_metadata(key)) {
