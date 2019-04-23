1baf35427902d7a64e9d2179570511a0f181aa16
Ubuntu <ubuntu@ip-172-31-7-219.ec2.internal>
2019-04-23 20:05:06 +0000

    Causal tweet replies, plus retwis tests.

--- functions/benchmarks/retwis_benchmark.py
+++ functions/benchmarks/retwis_benchmark.py
@@ -13,7 +13,6 @@ from anna.lattices import *
 
 def run(flconn, kvs):
 
-
     class FluentRedisShim:
         def __init__(self, fluent_user_library):
             self._fluent_lib = fluent_user_library
@@ -26,19 +25,33 @@ def run(flconn, kvs):
         ## Single value storage.
 
         # Retrieving arbitrary values that were stored by set().
+
         def get(self, key):
+            vc, value = self.get_with_vc(key)
+            return value
+
+        def get_with_vc(self, key):
             if self.exists(key):
-                values = self._fluent_lib.get(key)
+                vc, values = self._fluent_lib.get(key)
                 value = values[0]
                 value = pickle.loads(value)
-                return value
+                return vc, value
             else:
-                return None
+                return None, None
 
         # Storing arbitary values that can be retrieved by get().
         def set(self, key, value):
             value = pickle.dumps(value)
             self._fluent_lib.put(key, value)
+
+        # Do a set with causal dependencies.
+        # Dependencies is {'key': <vc>}
+        def causal_set(self, key, value, dependencies):
+            value = pickle.dumps(value)
+
+            client_id = str(int(uuid.uuid4()))
+            vector_clock = {client_id: 1}
+            self._fluent_lib.causal_put(key, vector_clock, dependencies, value, client_id)
 
         ## Counter storage.
 
@@ -64,7 +77,7 @@ def run(flconn, kvs):
         # Set contents.
         def smembers(self, key):
             if self.exists(key):
-                values = self._fluent_lib.get(key)
+                vc, values = self._fluent_lib.get(key)
                 return set((pickle.loads(val) for val in values))
             else:
                 return set()
@@ -92,7 +105,7 @@ def run(flconn, kvs):
         # Slice.
         def lrange(self, key, begin, end):
             if self.exists(key):
-                values = self._fluent_lib.get(key)
+                vc, values = self._fluent_lib.get(key)
                 oset = ListBasedOrderedSet(values)
                 values = [
                     # trim off timestamp + delimiter, and deserialize the rest.
@@ -106,7 +119,7 @@ def run(flconn, kvs):
         # Size.
         def llen(self, key):
             if self.exists(key):
-                values = self._fluent_lib.get(key)
+                vc, values = self._fluent_lib.get(key)
                 return len(list(values))
             else:
                 return 0
@@ -186,7 +199,11 @@ def run(flconn, kvs):
         _from, _to = timeline_len - page*10, timeline_len - (page-1)*10,
         timeline = r.lrange("user:id:%s:timeline" % userid, _from, _to)
         if timeline:
-          return [Post(r, int(post_id)).content for post_id in timeline]
+          # XXX waiting on causal consistency is done already,
+          # but we still need to reinsert the dependee tweet.
+          # We can skip this for now since the performance overhead is already incurred;
+          # inserting the actual tweets is just a quick operation for application-semantics correctness.
+          return [(int(post_id), Post(r, int(post_id)).content) for post_id in timeline]
         return []
 
       def mentions(self,page=1):
@@ -265,13 +282,33 @@ def run(flconn, kvs):
 
     class Post(Model):
       @staticmethod
-      def create(r, user, content):
+      def create(r, user, content, parent_post_id=None):
         userid = User.find_by_user(r, user)
         post_id = r.incr("post:uid")
         post = Post(r, post_id)
-        post.content = content
         post.user_id = userid
         # #post.created_at = Time.now.to_s
+
+        # Handle replies. If we're replying to a tweet,
+        # parent_content_key is the key of that tweet's content.
+        if parent_post_id is None:
+            post.content = content
+        else:
+            klass = post.__class__.__name__.lower()
+
+            # Convert the parent post id to its post content id.
+            parent_content_key = '%s:id:%s:%s' % (klass, parent_post_id, 'content')
+
+            # Get the parent tweet content's VC; we need it to express a dependency on it.
+            vc, _ = r.get_with_vc(parent_content_key)
+
+            # Post our tweet, with the causal dependency.
+            r.causal_set('%s:id:%s:%s' % (klass,post.id,'content'), content, {parent_content_key: vc})
+
+            # Set that this tweet has a parent (for application use, not kvs consistency).
+            post.parent = parent_post_id
+
+
         User.add_post(r, userid, post_id)
         # r.lpush("timeline", post_id)  # not testing global timeline
         for follower in User.followers(r, userid):
@@ -282,6 +319,9 @@ def run(flconn, kvs):
         #   u = User.find_by_user(mention[1:])
         #   if u:
         #     u.add_mention(post)
+
+
+
 
       @staticmethod
       def find_by_id(id):
@@ -296,66 +336,71 @@ def run(flconn, kvs):
 
 
 
-    def aaa_redis_exists(fluent, key):
+    def xxx_redis_exists(fluent, key):
         redis = FluentRedisShim(fluent)
         return str(redis.exists(key))
-    def aaa_redis_get(fluent, key):
+    def xxx_redis_get(fluent, key):
         redis = FluentRedisShim(fluent)
         return str(redis.get(key))
-    def aaa_redis_set(fluent, key, value):
+    def xxx_redis_set(fluent, key, value):
         redis = FluentRedisShim(fluent)
         redis.set(key, value)
         return 'success'
-    def aaa_redis_incr(fluent, key):
+    def xxx_redis_incr(fluent, key):
         redis = FluentRedisShim(fluent)
         return str(redis.incr(key))
-    def aaa_redis_sadd(fluent, key, value):
+    def xxx_redis_sadd(fluent, key, value):
         redis = FluentRedisShim(fluent)
         redis.sadd(key, value)
         return 'success'
-    def aaa_redis_smembers(fluent, key):
+    def xxx_redis_smembers(fluent, key):
         redis = FluentRedisShim(fluent)
         return str(redis.smembers(key))
-    def aaa_redis_lpush(fluent, key, value):
+    def xxx_redis_lpush(fluent, key, value):
         redis = FluentRedisShim(fluent)
         redis.lpush(key, value)
         return 'success'
-    def aaa_redis_lrange(fluent, key, begin, end):
+    def xxx_redis_lrange(fluent, key, begin, end):
         redis = FluentRedisShim(fluent)
         return str(redis.lrange(key, begin, end))
-    def aaa_redis_llen(fluent, key):
+    def xxx_redis_llen(fluent, key):
         redis = FluentRedisShim(fluent)
         return str(redis.llen(key))
 
 
 
 
-    # def aaa_global_timeline(fluent_lib, page):
+    # def xxx_global_timeline(fluent_lib, page):
     #     f_redis = FluentRedisShim(fluent_lib)
     #     return Timeline.page(f_redis, page)
 
-    def aaa_user_create(fluent_lib, user):
+    def xxx_user_create(fluent_lib, user):
         f_redis = FluentRedisShim(fluent_lib)
-        # fluent_lib.put('aaa_user', SetLattice({b'hi',}))
-        # fluent_lib.put('aaa_userb', OrderedSetLattice(ListBasedOrderedSet([b'hi'])))
+        # fluent_lib.put('xxx_user', SetLattice({b'hi',}))
+        # fluent_lib.put('xxx_userb', OrderedSetLattice(ListBasedOrderedSet([b'hi'])))
         User.create(f_redis, user, 'password')
         return 'success'
 
-    def aaa_user_timeline(fluent_lib, user, page):
+    def xxx_user_timeline(fluent_lib, user, page):
         f_redis = FluentRedisShim(fluent_lib)
         return User.timeline(f_redis, user, page)
 
-    def aaa_user_profile(fluent_lib, user, page):
+    def xxx_user_profile(fluent_lib, user, page):
         pass
 
-    def aaa_user_follow(fluent_lib, user, target):
+    def xxx_user_follow(fluent_lib, user, target):
         f_redis = FluentRedisShim(fluent_lib)
         User.follow(f_redis, user, target)
         return 'success'
 
-    def aaa_post_create(fluent_lib, user, post):
+    def xxx_post_create(fluent_lib, user, post):
         f_redis = FluentRedisShim(fluent_lib)
         Post.create(f_redis, user, post)
+        return 'success'
+
+    def xxx_reply_create(fluent_lib, user, post, parent_cid):
+        f_redis = FluentRedisShim(fluent_lib)
+        Post.create(f_redis, user, post, parent_cid)
         return 'success'
 
     # user, postcontent, pids -> ...
@@ -367,21 +412,22 @@ def run(flconn, kvs):
 
 
     fns = {
-        'aaa_redis_exists': aaa_redis_exists,
-        'aaa_redis_get': aaa_redis_get,
-        'aaa_redis_set': aaa_redis_set,
-        'aaa_redis_incr': aaa_redis_incr,
-        'aaa_redis_sadd': aaa_redis_sadd,
-        'aaa_redis_smembers': aaa_redis_smembers,
-        'aaa_redis_lpush': aaa_redis_lpush,
-        'aaa_redis_lrange': aaa_redis_lrange,
-        'aaa_redis_llen': aaa_redis_llen,
-        # 'aaa_global_timeline': aaa_global_timeline,
-        'aaa_user_create': aaa_user_create,
-        'aaa_user_timeline': aaa_user_timeline,
-        'aaa_user_profile': aaa_user_profile,
-        'aaa_user_follow': aaa_user_follow,
-        'aaa_post_create': aaa_post_create,
+        'xxx_redis_exists': xxx_redis_exists,
+        'xxx_redis_get': xxx_redis_get,
+        'xxx_redis_set': xxx_redis_set,
+        'xxx_redis_incr': xxx_redis_incr,
+        'xxx_redis_sadd': xxx_redis_sadd,
+        'xxx_redis_smembers': xxx_redis_smembers,
+        'xxx_redis_lpush': xxx_redis_lpush,
+        'xxx_redis_lrange': xxx_redis_lrange,
+        'xxx_redis_llen': xxx_redis_llen,
+        # 'xxx_global_timeline': xxx_global_timeline,
+        'xxx_user_create': xxx_user_create,
+        'xxx_user_timeline': xxx_user_timeline,
+        'xxx_user_profile': xxx_user_profile,
+        'xxx_user_follow': xxx_user_follow,
+        'xxx_post_create': xxx_post_create,
+        'xxx_reply_create': xxx_reply_create,
     }
 
     cfns = {
@@ -397,25 +443,44 @@ def run(flconn, kvs):
     def callfn(fname, *args):
         r = cfns[fname](*args).get()
         print("%s(%s) -> %s" % (fname, args, r))
+        return r
 
     # Redis shim tests (not retwis related).
-    callfn('aaa_redis_exists', 'aaa_foo')
-    callfn('aaa_redis_set', 'aaa_foo', b'3')
-    callfn('aaa_redis_get', 'aaa_foo')
-    callfn('aaa_redis_incr', 'aaa_cntr')
-    callfn('aaa_redis_sadd', 'aaa_sxt', b'4')
-    callfn('aaa_redis_smembers', 'aaa_sxt')
-    callfn('aaa_redis_lpush', 'aaa_lxt', b'5')
-    callfn('aaa_redis_lrange', 'aaa_lxt', 0, 10)
-    callfn('aaa_redis_llen', 'aaa_lxt')
-    callfn('aaa_redis_lpush', 'aaa_lxt', b'6')
-    callfn('aaa_redis_lpush', 'aaa_lxt', b'4')
-    callfn('aaa_redis_lrange', 'aaa_lxt', 0, 10)
+    callfn('xxx_redis_exists', 'xxx_foo')
+    callfn('xxx_redis_set', 'xxx_foo', b'3')
+    callfn('xxx_redis_get', 'xxx_foo')
+    callfn('xxx_redis_incr', 'xxx_cntr')
+    callfn('xxx_redis_sadd', 'xxx_sxt', b'4')
+    callfn('xxx_redis_smembers', 'xxx_sxt')
+    callfn('xxx_redis_lpush', 'xxx_lxt', b'5')
+    callfn('xxx_redis_lrange', 'xxx_lxt', 0, 10)
+    callfn('xxx_redis_llen', 'xxx_lxt')
+    callfn('xxx_redis_lpush', 'xxx_lxt', b'6')
+    callfn('xxx_redis_lpush', 'xxx_lxt', b'4')
+    callfn('xxx_redis_lrange', 'xxx_lxt', 0, 10)
+
+
+    # Retwis tests (not benchmark related).
+    callfn('xxx_user_create', 'xxx_alice')
+    callfn('xxx_user_create', 'xxx_bob')
+    callfn('xxx_user_create', 'xxx_obama')
+    callfn('xxx_user_follow', 'xxx_alice', 'xxx_obama')
+    callfn('xxx_user_follow', 'xxx_alice', 'xxx_bob')
+    callfn('xxx_user_follow', 'xxx_bob', 'xxx_obama')
+    callfn('xxx_post_create', 'xxx_obama', 'hello world!')
+    r = callfn('xxx_user_timeline', 'xxx_bob', 1)
+    obamas_tweet = r[-1]
+    obamas_tweet_cid = obamas_tweet[0]
+    callfn('xxx_reply_create', 'xxx_bob', 'hi obama!', obamas_tweet_cid)
+    callfn('xxx_user_timeline', 'xxx_alice', 1)
+    # return
+
+
 
     # Experiment parameters.
-    num_users = 10
-    max_degree = 3
-    num_pretweets = 100
+    num_users = 100
+    max_degree = 10
+    num_pretweets = 1000
     num_ops = 100  # 80% reads, 20% writes
     usernames = [str(i + 1) for i in range(num_users)]
 
@@ -441,9 +506,9 @@ def run(flconn, kvs):
     print ("Making users...")
     # Make all the users.
     for username in usernames:
-        res = cfns['aaa_user_create'](username).get()
+        res = cfns['xxx_user_create'](username).get()
         if res != 'success':
-            print("aaa_user_create(%s) -> %s" % (username, str(res)))
+            print("xxx_user_create(%s) -> %s" % (username, str(res)))
             sys.exit(1)
 
     # Make all the user connections.
@@ -454,17 +519,34 @@ def run(flconn, kvs):
     for username in usernames:
         targets = get_n_zipf_users(max_degree)
         for target in targets:
-            res = cfns['aaa_user_follow'](username, target).get()
+            res = cfns['xxx_user_follow'](username, target).get()
             if res != 'success':
-                print("aaa_user_follow(%s, %s) -> %s" % (username, target, str(res)))
+                print("xxx_user_follow(%s, %s) -> %s" % (username, target, str(res)))
                 sys.exit(1)
+
+    # Posts a reply to the most recent tweet in the user's timeline, if available.
+    # Does nothing if there are no tweets in the timeline to reply to!
+    def post_random_reply(username):
+        res = cfns['xxx_user_timeline'](username, 1).get()
+        if len(res) == 0: return False
+        parent_tweet_cid = res[-1][0]
+        res = cfns['xxx_reply_create'](
+            username, "{} says: @{}, agreed!".format(username, parent_tweet_cid), parent_tweet_cid).get()
+        return True
+
 
     # Prepopulating tweets, so our read and write times are more realistic.
     print ("Prepopulating tweets...")
     for _ in range(num_pretweets):
         username = get_random_user()
         post = "{} says: I love fluent!".format(username)
-        res = cfns['aaa_post_create'](username, post).get()
+        # Let's make some of these tweets be replies to other tweets.
+        t = random.random()
+        tweeted = False
+        if t < 0.2: # Reply attempt factor.
+            tweeted = post_random_reply(username)
+        if not tweeted:
+            cfns['xxx_post_create'](username, post).get()
 
     # Execute workload.
     rtimes = []
@@ -478,14 +560,19 @@ def run(flconn, kvs):
         # 80% reads.
         if t < 0.8:
             r_start = time.time()
-            res = cfns['aaa_user_timeline'](username, 1).get()
+            res = cfns['xxx_user_timeline'](username, 1).get()
             rtimes.append(time.time() - r_start)
 
         # 20% writes.
         else:
             w_start = time.time()
-            post = "{} says: I LOVE fluent!".format(username)
-            res = cfns['aaa_post_create'](username, post).get()
+            tweeted = False
+            reply_roll = random.random()
+            if reply_roll < 0.2:
+                tweeted = post_random_reply(username)
+            if not tweeted:
+                post = "{} says: I LOVE fluent!".format(username)
+                res = cfns['xxx_post_create'](username, post).get()
             wtimes.append(time.time() - w_start)
 
     end = time.time()
@@ -493,29 +580,29 @@ def run(flconn, kvs):
 
 
     # Sanity check: print timeline of most and least popular user.
-    res = cfns['aaa_user_timeline']('1', 1).get()
-    print("aaa_user_timeline('1', 1) -> %s" % (str(res)))
-    res = cfns['aaa_user_timeline'](str(num_users), 1).get()
-    print("aaa_user_timeline(%s, 1) -> %s" % (str(num_users), str(res)))
+    res = cfns['xxx_user_timeline']('1', 1).get()
+    print("xxx_user_timeline('1', 1) -> %s" % (str(res)))
+    res = cfns['xxx_user_timeline'](str(num_users), 1).get()
+    print("xxx_user_timeline(%s, 1) -> %s" % (str(num_users), str(res)))
 
 
 
 
 
-    # res = cfns['aaa_user_create']('bobaaa_').get()
-    # print("aaa_user_create('bobaaa_') -> %s" % (str(res)))
-    # res = cfns['aaa_user_create']('emilyaaa_').get()
-    # print("aaa_user_create('emilyaaa_') -> %s" % (str(res)))
-    # res = cfns['aaa_user_follow']('emilyaaa_', 'bobaaa_').get()
-    # print("aaa_user_follow('emilyaaa_', 'bobaaa_') -> %s" % (str(res)))
-    # res = cfns['aaa_post_create']('bobaaa_', 'im bob lol').get()
-    # print("aaa_post_create('bobaaa_', 'im bob lol') -> %s" % (str(res)))
-    # res = cfns['aaa_post_create']('emilyaaa_', 'im emily lol').get()
-    # print("aaa_post_create('emilyaaa_', 'im emily lol') -> %s" % (str(res)))
-    # res = cfns['aaa_user_timeline']('bobaaa_', 1).get()
-    # print("aaa_user_timeline('bobaaa_', 1) -> %s" % (str(res)))
-    # res = cfns['aaa_user_timeline']('emilyaaa_', 1).get()
-    # print("aaa_user_timeline('emilyaaa_', 1) -> %s" % (str(res)))
+    # res = cfns['xxx_user_create']('bobxxx_').get()
+    # print("xxx_user_create('bobxxx_') -> %s" % (str(res)))
+    # res = cfns['xxx_user_create']('emilyxxx_').get()
+    # print("xxx_user_create('emilyxxx_') -> %s" % (str(res)))
+    # res = cfns['xxx_user_follow']('emilyxxx_', 'bobxxx_').get()
+    # print("xxx_user_follow('emilyxxx_', 'bobxxx_') -> %s" % (str(res)))
+    # res = cfns['xxx_post_create']('bobxxx_', 'im bob lol').get()
+    # print("xxx_post_create('bobxxx_', 'im bob lol') -> %s" % (str(res)))
+    # res = cfns['xxx_post_create']('emilyxxx_', 'im emily lol').get()
+    # print("xxx_post_create('emilyxxx_', 'im emily lol') -> %s" % (str(res)))
+    # res = cfns['xxx_user_timeline']('bobxxx_', 1).get()
+    # print("xxx_user_timeline('bobxxx_', 1) -> %s" % (str(res)))
+    # res = cfns['xxx_user_timeline']('emilyxxx_', 1).get()
+    # print("xxx_user_timeline('emilyxxx_', 1) -> %s" % (str(res)))
 
 
 
