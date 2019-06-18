c8fc9f2dd781e5ed91c34351adc6a101cc383083
Vikram Sreekanti <vsreekanti@gmail.com>
2019-06-18 16:40:43 -0700

    Uses DaemonSets for Kubernetes deployments (#123)
    * Moves to using a k8s daemon set
    
    * Minor bug fixes
    
    * Removes old hash ring check, replaced by DaemonSets
    
    * Formatting fixes

--- k8s/remove_node.py
+++ k8s/remove_node.py
@@ -17,41 +17,21 @@ import os
 
 import boto3
 import kubernetes as k8s
-from util import *
+import util
 
 ec2_client = boto3.client('ec2', os.getenv('AWS_REGION', 'us-east-1'))
 
 
 def remove_node(ip, ntype):
-    client = init_k8s()
+    client, _ = util.init_k8s()
 
-    pod = get_pod_from_ip(client, ip)
+    pod = util.get_pod_from_ip(client, ip)
     hostname = 'ip-%s.ec2.internal' % (ip.replace('.', '-'))
 
-    node = client.list_node(label_selector='kubernetes.io/hostname=' +
-                                           hostname).items[0]
-
-    unique_id = node.metadata.labels['podid'].split('-')[-1]
-    podname = '%s-pod-%s' % (ntype, unique_id)
-    client.delete_namespaced_pod(name=podname, namespace=NAMESPACE,
+    podname = pod.metadata.name
+    client.delete_namespaced_pod(name=podname, namespace=util.NAMESPACE,
                                  body=k8s.client.V1DeleteOptions())
     client.delete_node(name=hostname, body=k8s.client.V1DeleteOptions())
 
-    prev_count = get_previous_count(client, ntype)
-    run_process(['./modify_ig.sh', ntype, str(prev_count - 1)])
-
-    if ntype == 'ebs':
-        vol_ids = list(map(lambda vol: vol.volume_id,
-                           filter(lambda vol: vol is not None,
-                                  map(lambda vol: vol.aws_elastic_block_store,
-                                      pod.spec.volumes))))
-
-        for vid in vol_ids:
-            vol = ec2_client.describe_volumes(VolumeIds=[vid])['Volumes'][0]
-
-            # wait for volume to no longer be in use
-            while vol['State'] == 'in-use':
-                vol = ec2_client.describe_volumes(
-                      VolumeIds=[vid])['Volumes'][0]
-
-            ec2_client.delete_volume(VolumeId=vid)
+    prev_count = util.get_previous_count(client, ntype)
+    util.run_process(['./modify_ig.sh', ntype, str(prev_count - 1)])
